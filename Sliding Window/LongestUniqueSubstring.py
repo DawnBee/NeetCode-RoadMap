a2860637d84c5016f2184c1c2376bcac7de033e4
DawnBee <phalanx882@gmail.com>
2024-09-29 19:33:19 +0800

    Update LongestUniqueSubstring.py

--- Sliding Window/LongestUniqueSubstring.py
+++ Sliding Window/LongestUniqueSubstring.py
@@ -1,3 +1,27 @@
 """
-Coming Soon :)
+*=Longest Substring Without Repeating Characters=*
+
+Given a string s, find the length of the longest 
+substring without repeating characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
 """
+def length_of_substring(s):
+    seen, left, res = set(), 0, 0
+
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+        seen.add(s[right])
+        res = max(res, right - left + 1)
+
+    return res
